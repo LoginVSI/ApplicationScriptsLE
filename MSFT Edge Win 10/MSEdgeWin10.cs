47179218bf6d605a5ba44b8c30ec6382f447bd64
Henri Koelewijn <h.koelewijn@loginvsi.com>
2021-10-27 10:40:36 +0200

    Fixes Edge script, so it becomes browser independent. Should work on Chrome was well

--- MSFT Edge Win 10/MSEdgeWin10.cs
+++ MSFT Edge Win 10/MSEdgeWin10.cs
@@ -11,57 +11,46 @@ public class MicrosoftEdge83 : ScriptBase
     {
         // Define environment variable to use with Workload
         var temp = GetEnvironmentVariable("TEMP");
-              
+
         // Define random integer
         var rand = new Random();
-        var RandomNumber = rand.Next(5,9);
-        
+        var RandomNumber = rand.Next(5, 9);
+
         // Download the VSIwebsite.zip from the appliance and unzip in the %temp% folder
         CopyFile(KnownFiles.WebSite, $"{temp}\\LoginPI\\vsiwebsite.zip", overwrite: true);
         UnzipFile($"{temp}\\LoginPI\\vsiwebsite.zip", $"{temp}\\LoginPI\\vsiwebsite", overWrite: true);
 
-/*        if(!(DirectoryExists($"{temp}\\LoginPI\\vsiwebsite")))
-        {
-            Log("Downloading File");
-            CopyFile(KnownFiles.WebSite, $"{temp}\\LoginPI\\vsiwebsite.zip");
-            UnzipFile($"{temp}\\LoginPI\\vsiwebsite.zip", $"{temp}\\LoginPI\\vsiwebsite");
-        }
-        else
-        {
-            Log("File already exists");
-        }
-*/        
         // Start Browser
-        Wait(seconds:33, showOnScreen:true, onScreenText:"Start Edge");
+        Wait(seconds: 3, showOnScreen: true, onScreenText: "Start Edge");
         StartBrowser();
         MainWindow.Maximize();
         Wait(RandomNumber);
-/*        
+        /*        
+                // Navigate web
+                Wait(seconds:3, showOnScreen:true, onScreenText:"NPR");
+                Navigate("https://apple.com");
+                Wait(RandomNumber);
+                Wait(RandomNumber);
+        */
         // Navigate web
-        Wait(seconds:3, showOnScreen:true, onScreenText:"NPR");
-        Navigate("https://apple.com");
-        Wait(RandomNumber);
-        Wait(RandomNumber);
-*/        
-        // Navigate web
-        Wait(seconds:3, showOnScreen:true, onScreenText:"Microsoft");
+        Wait(seconds: 3, showOnScreen: true, onScreenText: "Microsoft");
         Navigate("https://microsoft.com");
         Wait(RandomNumber);
         Wait(RandomNumber);
-/*        
-        // Navigate web
-        Wait(seconds:3, showOnScreen:true, onScreenText:"YouTube");
-        Navigate("https://youtube.com");
-        Wait(RandomNumber);
-        Wait(RandomNumber);
-*/
+        /*        
+                // Navigate web
+                Wait(seconds:3, showOnScreen:true, onScreenText:"YouTube");
+                Navigate("https://youtube.com");
+                Wait(RandomNumber);
+                Wait(RandomNumber);
+        */
         // Navigate to the local html file
         Navigate($"file:///{temp}/LoginPI/vsiwebsite/chromescript/logonpage.html");
         Wait(RandomNumber);
 
         // Click on the login button
         // Browser.FindWebComponentBySelector("button[id='logonbutton']").Click();
-        Wait(seconds:3, showOnScreen:true, onScreenText:"Click Logon Button");
+        Wait(seconds: 3, showOnScreen: true, onScreenText: "Click Logon Button");
         //FindWebComponentBySelector("button[id='logonbutton']").Click();
         Type("{TAB}");
         Wait(1);
@@ -82,30 +71,26 @@ public class MicrosoftEdge83 : ScriptBase
         Wait(2);
 
         // Select the videopage tab
-        Wait(seconds:3, showOnScreen:true, onScreenText:"Let's watch a video of a drive along the Platte River in Colorado");
+        Wait(seconds: 3, showOnScreen: true, onScreenText: "Let's watch a video of a drive along the Platte River in Colorado");
         Browser.FindWebComponentBySelector("a[id='videopage']").Click();
 
         // Watch video for 30 seconds
         Wait(60);
 
         // Navigate back to main homepage and Click on Article
-        //MainWindow.FindControl(className : "Button:ToolbarButton", title : "Back").Click();
-        // The following works with Edge to go back
-        MainWindow.FindControl(className : "Button:BackForwardButton", title : "Back").Click();
+        Browser.Back();
         Wait(2);
         Browser.FindWebComponentBySelector("a[id='articlepage']").Click();
         Wait(2);
 
         // Scroll through webpage
-        Wait(seconds:3, showOnScreen:true, onScreenText:"Browse a Web Page");
-        MainWindow.MoveMouseToCenter();
-        MouseDown();
-        MouseUp();
+        Wait(seconds: 3, showOnScreen: true, onScreenText: "Browse a Web Page");
+        MainWindow.Click();
         MainWindow.Type("{PAGEDOWN}".Repeat(2));
         MainWindow.Type("{PAGEUP}".Repeat(2));
 
         // Stop the browser
-        Wait(seconds:3, showOnScreen:true, onScreenText:"Stopping Browser");
+        Wait(seconds: 3, showOnScreen: true, onScreenText: "Stopping Browser");
         Wait(2);
         StopBrowser();
 
